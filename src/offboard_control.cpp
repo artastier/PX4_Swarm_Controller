861150d6d60ec65d19fa3cc1981a297ebe2e4fff
aastier <arthur.astier37@gmail.com>
2024-01-20 15:11:50 +0100

    Remove useless code

--- src/offboard_control.cpp
+++ src/offboard_control.cpp
@@ -38,13 +38,9 @@
  * @author Nuno Marques <nuno.marques@dronesolutions.io>
  */
 
-#include <px4_msgs/msg/offboard_control_mode.hpp>
-#include <px4_msgs/msg/trajectory_setpoint.hpp>
 #include <px4_msgs/msg/vehicle_command.hpp>
 #include <px4_msgs/srv/vehicle_command.hpp>
-#include <px4_msgs/msg/vehicle_control_mode.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <stdint.h>
 
 #include <chrono>
 #include <iostream>
@@ -53,18 +49,14 @@ using namespace std::chrono;
 using namespace std::chrono_literals;
 using namespace px4_msgs::msg;
 
+// TODO: Try to create a class such as waypoint or the embedded control node can derive from
 class OffboardControl : public rclcpp::Node {
 public:
     OffboardControl() : Node("leader_control") {
 
         const std::string name_space{this->get_namespace()};
-//        const std::string name_space{"/px4_2"};
-        RCLCPP_INFO(this->get_logger(), "Instantiating OffboardControl");
-        offboard_control_mode_publisher_ = this->create_publisher<OffboardControlMode>(
-                name_space + "/fmu/in/offboard_control_mode", 10);
-        trajectory_setpoint_publisher_ = this->create_publisher<TrajectorySetpoint>(
-                name_space + "/fmu/in/trajectory_setpoint",
-                10);
+
+        // TODO: Check how to set sim_time to true
         arm_check_client_ = this->create_client<px4_msgs::srv::VehicleCommand>(
                 name_space + "/fmu/vehicle_command");
 
@@ -73,15 +65,6 @@ public:
             RCLCPP_ERROR(this->get_logger(), "Service not available");
         }
         RCLCPP_ERROR(this->get_logger(), "Vehicle command service available");
-        auto timer_callback = [this]() -> void {
-//            PX4 requires that the vehicle is already receiving OffboardControlMode messages before it will arm
-//            in offboard mode, or before it will switch to offboard mode when flying.
-
-            // offboard_control_mode needs to be paired with trajectory_setpoint
-            publish_offboard_control_mode();
-            publish_trajectory_setpoint();
-        };
-        timer_ = this->create_wall_timer(100ms, timer_callback);
         // Set to onboard_mode
         offboard_mode();
         // Set to onboard_mode
@@ -97,20 +80,12 @@ public:
     void disarm();
 
 private:
-    rclcpp::TimerBase::SharedPtr timer_;
 
-    rclcpp::Publisher<OffboardControlMode>::SharedPtr offboard_control_mode_publisher_;
-    rclcpp::Publisher<TrajectorySetpoint>::SharedPtr trajectory_setpoint_publisher_;
     rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedPtr arm_check_client_;
     const std::size_t max_client_connexions_try{100};
     bool is_armed{false};
     bool is_offboard{false};
 
-    std::atomic<uint64_t> timestamp_;   //!< common synced timestamped
-
-    void publish_offboard_control_mode();
-
-    void publish_trajectory_setpoint();
 
     bool request_vehicle_command(uint16_t command, float param1 = 0.0, float param2 = 0.0);
 };
@@ -120,9 +95,9 @@ private:
  */
 void OffboardControl::arm() {
     is_armed = request_vehicle_command(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0);
-    if (is_armed){
+    if (is_armed) {
         RCLCPP_INFO(this->get_logger(), "Drone armed !");
-    }else{
+    } else {
         RCLCPP_INFO(this->get_logger(), "Didn't succeed to arm the drone");
     }
 }
@@ -132,9 +107,9 @@ void OffboardControl::arm() {
  */
 void OffboardControl::offboard_mode() {
     is_offboard = request_vehicle_command(VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 6);
-    if (is_offboard){
+    if (is_offboard) {
         RCLCPP_INFO(this->get_logger(), "Drone is in offboard mode !");
-    }else{
+    } else {
         RCLCPP_INFO(this->get_logger(), "Didn't succeed to switch to offboard mode");
     }
 }
@@ -146,34 +121,6 @@ void OffboardControl::disarm() {
     request_vehicle_command(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0);
 
     RCLCPP_INFO(this->get_logger(), "Disarm command send");
-}
-
-/**
- * @brief Publish the offboard control mode.
- *        For this example, only position and altitude controls are active.
- */
-void OffboardControl::publish_offboard_control_mode() {
-    OffboardControlMode msg{};
-    msg.position = true;
-    msg.velocity = false;
-    msg.acceleration = false;
-    msg.attitude = false;
-    msg.body_rate = false;
-    msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-    offboard_control_mode_publisher_->publish(msg);
-}
-
-/**
- * @brief Publish a trajectory setpoint
- *        For this example, it sends a trajectory setpoint to make the
- *        vehicle hover at 5 meters with a yaw angle of 180 degrees.
- */
-void OffboardControl::publish_trajectory_setpoint() {
-    TrajectorySetpoint msg{};
-    msg.position = {0.0, 0.0, -5.0};
-    msg.yaw = -3.14; // [-PI:PI]
-    msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-    trajectory_setpoint_publisher_->publish(msg);
 }
 
 /**
