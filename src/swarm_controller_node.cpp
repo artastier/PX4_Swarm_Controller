0efa041a67868a208f1be1a398115287b0e1f105
aastier <arthur.astier37@gmail.com>
2024-02-08 21:48:34 +0100

    Made the timer callback virtual and remove the parameters from the mother class

--- src/swarm_controller_node.cpp
+++ src/swarm_controller_node.cpp
@@ -5,7 +5,6 @@
 #include <rclcpp/rclcpp.hpp>
 #include <chrono>
 #include <px4_msgs/msg/trajectory_setpoint.hpp>
-#include <px4_msgs/msg/vehicle_local_position.hpp>
 #include <px4_msgs/msg/offboard_control_mode.hpp>
 #include <custom_msgs/msg/neighbors.hpp>
 
@@ -13,7 +12,6 @@ using namespace std::chrono_literals;
 
 class SwarmController : public rclcpp::Node {
     using TrajectorySetpoint = px4_msgs::msg::TrajectorySetpoint;
-    using VehicleLocalPosition = px4_msgs::msg::VehicleLocalPosition;
 public:
     using Neighbors = custom_msgs::msg::Neighbors;
     using OffboardControlMode = px4_msgs::msg::OffboardControlMode;
@@ -26,38 +24,18 @@ private:
 
     virtual void neighbors_callback(const Neighbors::SharedPtr &neighbors) = 0;
 
+    virtual void timer_callback() = 0;
+
 protected:
     rclcpp::Publisher<OffboardControlMode>::SharedPtr offboard_control_mode_publisher_;
     rclcpp::Publisher<TrajectorySetpoint>::SharedPtr trajectory_setpoint_publisher_;
     rclcpp::Subscription<Neighbors>::SharedPtr neighbors_subscriber;
     rclcpp::TimerBase::SharedPtr timer;
 
-    std::size_t drone_id{};
-    std::size_t nb_drones{};
-
-    double x_init{};
-    double y_init{};
-
-    double x_formation{};
-    double y_formation{};
-
-    // TODO: Check to make a daughter class from this one
-
-
 };
 
 SwarmController::SwarmController() : rclcpp::Node("swarm_controller") {
     const std::string name_space{this->get_namespace()};
-    this->declare_parameter<int>("drone_id");
-    this->declare_parameter<int>("nb_drones");
-    this->declare_parameter<double>("x_init");
-    this->declare_parameter<double>("y_init");
-
-    x_init = this->get_parameter("x_init").as_double();
-    y_init = this->get_parameter("y_init").as_double();
-
-    drone_id = static_cast<std::size_t>(this->get_parameter("drone_id").as_int());
-    nb_drones = static_cast<std::size_t>(this->get_parameter("nb_drones").as_int());
 
     offboard_control_mode_publisher_ = this->create_publisher<OffboardControlMode>(
             name_space + "/fmu/in/offboard_control_mode", 10);
@@ -68,10 +46,7 @@ SwarmController::SwarmController() : rclcpp::Node("swarm_controller") {
                                                                 [this](const Neighbors::SharedPtr neighbors) {
                                                                     neighbors_callback(neighbors);
                                                                 });
-    auto timer_callback = [this]() {
-        publish_offboard_control_mode();
-        // Check to send a default position setpoint and when receiving neighbors using the control law
-    };
-    timer = this->create_wall_timer(100ms, timer_callback);
+
+    timer = this->create_wall_timer(100ms, [this](){timer_callback();});
 }
 
--- src/weighted_topology_controller.cpp
+++ src/weighted_topology_controller.cpp
@@ -1,13 +1,22 @@
 /**
-* @author Arthur Astier
+ * @brief  This controller is based on the paper:
+ *
+ * Distributed leader-follower formation control for multiple quadrotors with weighted topology,
+ *
+ * Zhicheng Hou, Isabelle Fantoni, hal-01180491
+ *
+ * @author Arthur Astier
 */
 #include <rclcpp/rclcpp.hpp>
+#include <px4_msgs/msg/vehicle_local_position.hpp>
+#include <eigen3/Eigen/Eigen>
 #include "swarm_controller_node.cpp"
 
 class WeightedTopologyController : public SwarmController {
     using OffboardControlMode = SwarmController::OffboardControlMode;
     using Neighbors = SwarmController::Neighbors;
-    // TODO: Check to define Neighbors type
+    using PRCS = Eigen::Vector<std::size_t, Eigen::Dynamic>;
+    using Weights = Eigen::VectorXd;
 public:
     WeightedTopologyController();
 
@@ -15,10 +24,37 @@ private:
     void publish_offboard_control_mode() override;
 
     void neighbors_callback(const Neighbors::SharedPtr &neighbors) override;
+
+    void timer_callback() override;
+
+private:
+    PRCS prcs;
+    Weights weights;
+
+    std::size_t drone_id{};
+    std::size_t nb_drones{};
+
+    double x_init{};
+    double y_init{};
+
 };
 
 WeightedTopologyController::WeightedTopologyController() : SwarmController() {
+    // TODO: Add the distances of all the drones to the leader as a parameter
+    this->declare_parameter<int>("drone_id");
+    this->declare_parameter<int>("nb_drones");
+    this->declare_parameter<double>("x_init");
+    this->declare_parameter<double>("y_init");
 
+
+    x_init = this->get_parameter("x_init").as_double();
+    y_init = this->get_parameter("y_init").as_double();
+
+    drone_id = static_cast<std::size_t>(this->get_parameter("drone_id").as_int());
+    nb_drones = static_cast<std::size_t>(this->get_parameter("nb_drones").as_int());
+
+    prcs = PRCS::Zero(nb_drones);
+    weights = Weights::Zero(nb_drones);
 }
 
 /**
@@ -26,10 +62,10 @@ WeightedTopologyController::WeightedTopologyController() : SwarmController() {
  */
 void WeightedTopologyController::publish_offboard_control_mode() {
     OffboardControlMode msg{};
-    msg.position = true;
-    // TODO: Check if velocity control is needed
+    msg.position = false;
     msg.velocity = false;
-    msg.acceleration = false;
+    // In the paper, the command is a command of acceleration
+    msg.acceleration = true;
     msg.attitude = false;
     msg.body_rate = false;
     msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
@@ -42,6 +78,14 @@ void WeightedTopologyController::publish_offboard_control_mode() {
  */
 void WeightedTopologyController::neighbors_callback(const Neighbors::SharedPtr &neighbors) {
 
+}
+
+/**
+ * @brief
+ */
+void WeightedTopologyController::timer_callback() {
+    // TODO: Handle when no neighbors are detected (at the beginning for example)
+    publish_offboard_control_mode();
 }
 
 /**
